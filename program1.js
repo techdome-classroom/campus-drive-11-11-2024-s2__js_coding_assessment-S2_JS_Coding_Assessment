d5c3501283a80c7bba35ec58292f5e6a159f9b2b
Hritesh Shanty <shanty.hritesh@gmail.com>
2024-11-11 15:33:45 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -16,7 +16,6 @@ var isValid = function(s) {
             stklen-=1
         }
         else if (s[i]===']' && stk[stklen-1]==='['){
-            while (stk[stklen-1]==='['){
             stk.pop()
             stklen-=1
         }
