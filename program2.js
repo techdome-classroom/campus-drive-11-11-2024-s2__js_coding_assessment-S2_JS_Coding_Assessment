b225febde908e54f613fa598cfa5fd0bbb719a0d
Hritesh Shanty <shanty.hritesh@gmail.com>
2024-11-11 16:00:03 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -6,7 +6,7 @@ var romanToInt = function(s) {
     const hm={I:1, V:5, X:10, L :50, C :100, D :500, M :1000}
     let n=s.length
     let num=hm[s[0]]
-    for (let i=0;i<n;i++){
+    for (let i=1;i<n;i++){
         
     }
 };
