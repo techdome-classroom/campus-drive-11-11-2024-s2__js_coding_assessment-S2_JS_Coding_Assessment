c3004a874c01f106e55f6c1af1d4a0cd77152b64
kranthi kumar dande <139384937+kranthikeya@users.noreply.github.com>
2024-11-11 16:12:00 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,10 +1,27 @@
 /**
  * @param {string} s
- * @return {number}
+ * @return {boolean}
  */
-var romanToInt = function(s) {
-    
+var isValid = function(s) {
+    const stack = [];
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char in bracketMap) {  // If it's a closing bracket
+            const topElement = stack.length > 0 ? stack.pop() : '#'; // Pop from stack or use dummy value
+            if (bracketMap[char] !== topElement) {
+                return false;
+            }
+        } else {  // If it's an opening bracket
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0; // Stack should be empty if all brackets are matched
 };
 
-
-module.exports={romanToInt}
+module.exports = { isValid };
