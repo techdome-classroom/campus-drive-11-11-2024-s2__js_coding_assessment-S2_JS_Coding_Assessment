38c27f76d4066f8fbd7ea4a72492e7002a89ecfe
Hritesh Shanty <shanty.hritesh@gmail.com>
2024-11-11 15:21:55 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let n=len(s)
+    const stk=[]
+    let stklen=0
+    for (let i=0;i<n;i++){
+        if (s[i]==='(' || s[i]==='[' || s[i]=='{'){
+            stk.push(s[i])
+            stklen+=1
+        }
+        else if (s[i]===')'){
+            while (stk[stklen-1]==='('){
+                stk.pop()
+                stklen-=1
+            }
+        }
+        else if (s[i]===']'){
+            while (stk[stklen-1]==='['){
+                stk.pop()
+                stklen-=1
+            }
+        }
+        else{
+            while (stk[stklen-1]==='{'){
+                stk.pop()
+                stklen-=1
+            }
+        }
+    }
+    return !stk;
 };
 
 module.exports = { isValid };
